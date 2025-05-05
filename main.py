c176d4028ba584d7b47dd7acd05ea7555a799e25
Maria Gomez <maga1731@gmail.com>
2025-05-05 11:24:47 -0500

    feat :tada: Intro Python

---
+++ main.py
@@ -0,0 +1,26 @@
+#####################
+##### Clase Dia 1 ###
+#####################
+
+#Imprimir en Python
+
+print ("Hola")
+
+
+
+#Creacion de variables
+#1. Dato tipo string
+nombre= "Maria"
+print(type(nombre))
+
+#2. Dato tipo numero entero
+numeroEntero=5
+print(type(numeroEntero))
+
+#3. Dato tipo numero real
+numeroReal = 5.3
+print(type(numeroReal))
+
+
+
+#Desarrollado por: Maria Alejandra Gomez Archila - CC.1005234916
